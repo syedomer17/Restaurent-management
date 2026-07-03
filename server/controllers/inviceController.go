2113e25db6c50d95cc719e731a3955550e61be5a
syedomer17 <syedomerali2006@gmail.com>
2026-07-03 19:02:21 +0530

    invice controller apis done and the orderitem controller is done half

--- server/controllers/inviceController.go
+++ server/controllers/inviceController.go
@@ -9,7 +9,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type InvoiceViewFormatter struct {
@@ -82,13 +84,109 @@ func GetInvoice() gin.HandlerFunc {
 
 func CreateInvoice() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		var invoice models.Invoice
+
+		if err := c.BindJSON(&invoice); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var order models.Order 
+
+		err := orderCollection.FindOne(ctx, bson.M{"order_id": invoice.Order_id}).Decode(&order)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Order not found for the given order_id"})
+			return
+		}
+
+		status := "PENDING"
+		if invoice.Payment_status == nil {
+			invoice.Payment_status = &status
+		}
+
+		invoice.Payment_due_date, _ = time.Parse(time.RFC3339, time.Now().AddDate(0,0, 1).Format(time.RFC3339))
+		invoice.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		invoice.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		invoice.ID = primitive.NewObjectID()
+		invoice.Invoice_id = invoice.ID.Hex()
+
+		validationErr := validate.Struct(invoice)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		result, insertErr := invoiceCollection.InsertOne(ctx, invoice)
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Invoice was not created"})
+			return
+		}
+		defer cancel()
+
+		c.JSON(http.StatusOK, result)
 	}
 }
 
 func UpdateInvoice() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		var invoice models.Invoice
+		invoiceId := c.Param("invoice_id")
+
+		if err := c.BindJSON(&invoice); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		filter := bson.M{"invoice_id": invoiceId}
+		var updateObj primitive.D
+
+		if invoice.Payment_method != nil {
+			updateObj = append(updateObj, bson.E{Key: "payment_method", Value: invoice.Payment_method})
+		}
+		
+		if invoice.Payment_status != nil {
+			updateObj = append(updateObj, bson.E{Key: "payment_status", Value: invoice.Payment_status})
+		}
+
+		invoice.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{Key: "updated_at", Value: invoice.Updated_at})
+
+		upsert := true 
+
+		options := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		satatus := "PENDING"
+
+		if invoice.Payment_status != nil {
+			invoice.Payment_status = &satatus
+		}
+
+		result, err := invoiceCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{"$set": updateObj},
+			},
+			&options,
+		)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Invoice update failed"})
+			return
+		}
+		defer cancel()
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Invoice updated successfully",
+			"result":  result,
+		})
 	}
 }
 
--- server/controllers/orderitemController.go
+++ server/controllers/orderitemController.go
@@ -1,12 +1,44 @@
 package controllers
 
 import (
+	"context"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/models"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+type OrderItemPack struct {
+	Table_id *string
+	Order_items []models.OrderItem
+}
+
+var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItems")
+
 
 func GetOrderItems() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err :=orderItemCollection.Find(context.TODO(), bson.M{})
+
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while fetching the order items"})
+			return
+		}
+		var allOrderItems []bson.M
+
+		if err = result.All(ctx, &allOrderItems); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while fetching the order items"})
+			return
+		}
+
+		c.JSON(http.StatusOK, allOrderItems)
 	}
 }
 
